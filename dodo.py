c5287c2b0ae0b2e97b70c0406a516488f226a633
Bryan Weber <bryan.w.weber@gmail.com>
2021-06-30 17:15:10 -0400

    Start generating my own figures
    Add a Python script to rewrite math in the text of the SVG files

---
+++ dodo.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+from raw_svg import render_math_svg
+
+HERE = Path(__file__).parent
+DOIT_CONFIG = {"default_tasks": ["build_jb"]}
+
+
+def task_svg_math():
+    for svg_file in HERE.glob("raw_svg/*.svg"):
+        target = render_math_svg.OUTPUT / svg_file.name
+        yield {
+            "name": svg_file.name,
+            "actions": [(render_math_svg.main, [svg_file])],
+            "targets": [target],
+            "file_dep": [svg_file, render_math_svg.__file__],
+        }
+
+
+def task_clean_svg_cache():
+    return {
+        "actions": [(render_math_svg.clean_cache)],
+        "file_dep": [render_math_svg.CACHE.resolve()],
+    }
+
+
+def task_build_jb():
+    return {
+        "actions": [["jb", "build", "."]],
+        "task_dep": ["svg_math"],
+        "uptodate": [False],
+        "verbosity": 2,
+    }
---
+++ raw_svg/render_math_svg.py
@@ -0,0 +1,193 @@
+from __future__ import annotations
+import requests
+from xml.etree import ElementTree as ET
+from pathlib import Path
+import json
+from typing import Optional, Union, Dict
+import sys
+from datetime import datetime
+
+HERE = Path(__file__).parent
+OUTPUT = HERE.parent / "images"
+# HOST = "https://math.vercel.app"
+HOST = "http://localhost:3000"
+PARAMS: dict[str, Optional[Union[str, bytes]]] = {"inline": None}
+CACHE = HERE / "mathjax_cache.json"
+CACHE_T = Dict[Optional[Union[str, bytes]], str]
+SESSION = requests.Session()
+NAMESPACES = {
+    "": "http://www.w3.org/2000/svg",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "cc": "http://creativecommons.org/ns#",
+    "dc": "http://purl.org/dc/elements/1.1/",
+    "bx": "https://boxy-svg.com",
+}
+CC_META = """
+<root xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:cc="http://creativecommons.org/ns#" xmlns:dc="http://purl.org/dc/elements/1.1/">
+<metadata>
+  <rdf:RDF>
+    <cc:Work rdf:about="">
+      <cc:license rdf:resource="http://creativecommons.org/licenses/by-sa/4.0/" />
+      <dc:title></dc:title>
+      <dc:date></dc:date>
+      <dc:format>image/svg+xml</dc:format>
+      <dc:creator>
+        <cc:Agent>
+          <dc:title>Bryan Weber</dc:title>
+        </cc:Agent>
+      </dc:creator>
+   </cc:Work>
+   <cc:License
+      rdf:about="http://creativecommons.org/licenses/by-sa/4.0/">
+      <cc:permits
+         rdf:resource="http://creativecommons.org/ns#Reproduction" />
+      <cc:permits
+         rdf:resource="http://creativecommons.org/ns#Distribution" />
+      <cc:requires
+         rdf:resource="http://creativecommons.org/ns#Notice" />
+      <cc:requires
+         rdf:resource="http://creativecommons.org/ns#Attribution" />
+      <cc:permits
+         rdf:resource="http://creativecommons.org/ns#DerivativeWorks" />
+      <cc:requires
+         rdf:resource="http://creativecommons.org/ns#ShareAlike" />
+   </cc:License>
+   </rdf:RDF>
+</metadata>
+</root>
+"""
+
+
+def load_cache() -> CACHE_T:
+    if CACHE.exists():
+        cache = json.loads(CACHE.read_text())
+    else:
+        cache = {}
+    return cache
+
+
+def write_cache(cache: CACHE_T) -> None:
+    if cache:
+        CACHE.write_text(json.dumps(cache))
+
+
+def get_math_content(elem: ET.Element, cache: CACHE_T, s: requests.Session) -> str:
+    value = elem.text
+    if value is None:
+        raise ValueError("Text of node cannot be 'None'", elem)
+    if value in cache:
+        content = cache[value]
+    else:
+        PARAMS["inline"] = value
+        r = s.get(HOST, params=PARAMS)
+        r.raise_for_status()
+        content = r.content.decode("utf-8")
+        cache[value] = content
+    return content
+
+
+def clean_cache() -> None:
+    if CACHE.exists():
+        CACHE.unlink()
+
+
+def main(svg_file: Optional[Path] = None) -> None:
+    if svg_file is None:
+        for svg_f in HERE.glob("*.svg"):
+            main(svg_f)
+
+    # This is here for typing. Without this, mypy complains that svg_file might be None
+    assert svg_file is not None, "svg_file cannot be None at this point"
+
+    for prefix, uri in NAMESPACES.items():
+        ET.register_namespace(prefix, uri)
+
+    # The 'root' element is only needed to declare the namespaces for parsing,
+    # so retrieve just the metadata element here.
+    CC_XML = ET.fromstring(CC_META).find("metadata")
+    if CC_XML is None:
+        raise ValueError("Something went horribly wrong finding the metadata")
+
+    RDF = CC_XML.find("rdf:RDF", NAMESPACES)
+    if RDF is None:
+        raise ValueError("Could not find RDF tag")
+    Work = RDF.find("cc:Work", NAMESPACES)
+    if Work is None:
+        raise ValueError("Could not find Work tag")
+    title = Work.find("dc:title", NAMESPACES)
+    if title is None:
+        ET.SubElement(Work, "dc:title", text=svg_file.name)
+    else:
+        title.text = svg_file.name
+    date = Work.find("dc:date", NAMESPACES)
+    today = datetime.today().isoformat()
+    if date is None:
+        ET.SubElement(Work, "dc:date", text=today)
+    else:
+        date.text = today
+
+    cache = load_cache()
+    assert OUTPUT.exists() and OUTPUT.is_dir(), "The 'images' directory must exist"
+
+    svg_tree = ET.parse(svg_file)
+    svg_element = svg_tree.getroot()
+    if svg_element.get("height") is None or svg_element.get("width") is None:
+        view_box = svg_element.get("viewBox")
+        if view_box is None:
+            raise ValueError("svg element must have height and width or viewBox")
+        _, _, svg_width, svg_height = view_box.split()
+        svg_element.set("width", svg_width + "px")
+        svg_element.set("height", svg_height + "px")
+
+    defs_elem = svg_element.find("defs", NAMESPACES)
+    if defs_elem is not None:
+        font_def = defs_elem.find("style", NAMESPACES)
+        if font_def is not None:
+            defs_elem.remove(font_def)
+
+    svg_element.insert(1, CC_XML)
+
+    for elem in svg_tree.findall("text", NAMESPACES):
+        if "math" not in elem.get("class", ""):
+            continue
+        content = get_math_content(elem, cache, SESSION)
+
+        x_loc = elem.get("x")
+        y_loc = elem.get("y")
+        if x_loc is None or y_loc is None:
+            raise ValueError("Both x and y must be set")
+
+        math = ET.fromstring(content)
+        width = math.get("width", "auto")
+        height = math.get("height", "auto")
+        image_element = ET.Element(
+            "image",
+            attrib=dict(
+                x=x_loc,
+                y=y_loc,
+                height=height,
+                width=width,
+                href=f"data:image/svg+xml;utf8,{ET.tostring(math, encoding='unicode', xml_declaration=False)}",
+            ),
+        )
+        image_element.tail = "\n  "
+        svg_element.append(image_element)
+        svg_element.remove(elem)
+
+    svg_tree.write(
+        OUTPUT.joinpath(svg_file.name), encoding="unicode", xml_declaration=True
+    )
+
+    write_cache(cache)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1]:
+        svg_file = Path(sys.argv[1])
+        if svg_file.exists():
+            main(svg_file)
+        else:
+            print("The given file does not exist", file=sys.stderr)
+            sys.exit(1)
+    else:
+        main()
